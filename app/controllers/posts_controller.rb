a3fba182e77fbb7c7af1ae12ae11f7804f30e316
sato akihide <aki@sato.local>
2019-06-18 16:28:02 +0900

    Add: 感想の投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    book = current_user.books.find_by(publication_id: params[:post][:publication_id])
+    @post = book.posts.build(post_params)
+
+    if @post.save
+      flash[:success] = "新規投稿しました。"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
