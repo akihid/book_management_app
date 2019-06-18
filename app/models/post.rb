3c438b9ca4ebfd60fd2f80f77b3ce5dea1fe1d62
sato akihide <aki@sato.local>
2019-06-18 16:49:29 +0900

    Fix: postモデルにdelegateを定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :book
-  # has_many :publications, through: :books
+  delegate :publication, to: :book
 
   validates :title , presence: true , length: {maximum:20}
   validates :content , presence: true , length: {maximum:200}
