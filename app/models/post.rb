b1c117c2c1234b848b1c82d6e8ee956270a66c10
sato akihide <aki@sato.local>
2019-06-18 16:44:51 +0900

    Add: 感想一覧に本の画像を表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :book
+  # has_many :publications, through: :books
 
   validates :title , presence: true , length: {maximum:20}
   validates :content , presence: true , length: {maximum:200}
