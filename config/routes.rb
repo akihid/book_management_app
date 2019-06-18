260cf7484508040efc62d2a197c77b7fe0c9c14d
sato akihide <aki@sato.local>
2019-06-19 07:42:39 +0900

    Add: post配下にcommentsのルート定義

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only:[:show]
   resources :books
   resources :posts do
+    resources :comments
     collection do
       post :confirm
     end
