f5fe1a7807bc3b34f949aedf90dcfd35deef40c5
sato akihide <aki@sato.local>
2019-06-18 17:37:00 +0900

    Add: 確認画面の戻る機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
   end
 
   def confirm
