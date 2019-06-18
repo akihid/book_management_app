77ed8b56b67dca3cffc5ea8a11177a56f6d718d8
sato akihide <aki@sato.local>
2019-06-18 14:03:50 +0900

    Add: Bookの削除処理実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[edit update]
+  before_action :set_book, only: %i[edit update destroy]
   def index
     @books = current_user.books
   end
@@ -27,6 +27,13 @@ class BooksController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    # make check authority
+    @book.destroy
+    flash[:danger] = "#{@book.publication.title}の削除完了"
+    redirect_to books_path
   end
 
   private
