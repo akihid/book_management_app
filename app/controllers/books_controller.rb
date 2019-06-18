fdb3dd25d5b6d81bdff01cd0ecc1346c97fa6b0a
sato akihide <aki@sato.local>
2019-06-18 13:00:06 +0900

    Fix: 本の登録エラーをページ遷移前に行うように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,15 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = current_user.books.build(publication_id: params[:publication])
+    # ユーザーの本を探し存在する場合はもとページに戻す
+    @book = Book.find_or_initialize_by(user_id: current_user.id, publication_id: params[:publication])
+    if @book.persisted?
+      redirect_back(fallback_location: request.url)
+      flash[:danger] = "#{params[:title]}はすでに持っているため登録できません"
+      # redirect_to publications_path
+    # else
+    #   @book = current_user.books.build(publication_id: params[:publication])
+    end
   end
 
   def create
@@ -12,9 +20,9 @@ class BooksController < ApplicationController
     if @book.save
       flash[:success] = '本の登録完了'
       redirect_to books_path
-    else
-      flash[:danger] = '登録済です'
-      redirect_to publications_path
+    # else
+    #   flash[:danger] = '登録済です'
+    #   redirect_to publications_path
     end
   end
 
