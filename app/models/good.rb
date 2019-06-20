3ab805f8dde2b9d7163aabf93577ccc8caeda984
sato akihide <aki@sato.local>
2019-06-20 11:35:43 +0900

    Add: いいねの数を実装

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,4 +1,4 @@
 class Good < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, counter_cache: :goods_count
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_055553) do
+ActiveRecord::Schema.define(version: 2019_06_20_022852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_055553) do
     t.bigint "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "goods_count", default: 0
     t.index ["book_id"], name: "index_posts_on_book_id"
   end
 
