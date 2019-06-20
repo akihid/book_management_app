ad56882c99a917ba0cdafb111c350055b3e79c59
sato akihide <aki@sato.local>
2019-06-20 21:22:19 +0900

    Fix: root_pathをposts#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'publications#index'
+  root 'posts#index'
   devise_for :users, controllers: { registrations: 'registrations' }
   
   resources :publications
