7b65dcb32c262fcfdbc144b23f85f5e98dbe6ca2
sato akihide <aki@sato.local>
2019-06-23 19:49:54 +0900

    Add: 感想の編集テスト作成

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe '感想CRUD機能' , type: :system do
   let(:user_a) {FactoryBot.create(:user , name: 'ユーザーA' , email: 'a@co.jp')}
+  let(:user_b) {FactoryBot.create(:user , name: 'ユーザーB' , email: 'b@co.jp')}
 
   3.times do| num| 
     let!("publication_#{num}") { FactoryBot.create(:publication, title: "出版物#{num}", isbn_code: "123456789000#{num}") }
@@ -10,6 +11,8 @@ describe '感想CRUD機能' , type: :system do
   let!(:book_0) { FactoryBot.create(:book, user: user_a, publication: publication_0) }
   let!(:book_1) { FactoryBot.create(:book, user: user_a, publication: publication_1) }
   let!(:book_2) { FactoryBot.create(:book, user: user_a, publication: publication_2) }
+  let!(:book_b) { FactoryBot.create(:book, user: user_b, publication: publication_0) }
+  let!(:post_b) { FactoryBot.create(:post, book: book_b) }
 
   before do
     visit user_session_path
@@ -47,6 +50,40 @@ describe '感想CRUD機能' , type: :system do
       click_on '修正する'
       expect(page).to have_content '感想の詳細確認です'
       expect(page).to have_content '出版物2'
+    end
+  end
+
+  describe '編集機能' do
+    let(:login_user) {user_a}
+
+    before do 
+      visit new_post_path
+      fill_in 'post_title' , with: '感想のタイトルです'
+      fill_in 'post_content' , with: '感想の詳細です'
+      click_on '登録'
+      click_on '登録'
+      page.first('#edit_post').click
+    end
+
+    it '編集後、一覧画面に更新された内容が表示される' do
+      fill_in 'post_title' , with: '感想のタイトル更新です'
+      fill_in 'post_content' , with: '感想の詳細更新です'
+      click_on '登録'
+      expect(page).to have_content '感想のタイトル更新です'
+    end
+  end
+
+
+  describe '一覧の表示確認' do
+    let(:login_user) {user_a}
+
+    before do 
+      visit posts_path
+    end
+
+    it '異なるユーザーが投稿した感想の編集ボタンは表示されない' do
+      expect(page).to have_content 'ユーザーB'
+      expect(page).not_to have_content '編集'
     end
   end
 end
