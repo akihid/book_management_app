bbb22cafad205f855f9dbbdcbbd970c8f6cf4e09
sato akihide <aki@sato.local>
2019-06-24 04:19:38 +0900

    Fix: passwordの更新ができなかったため修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :goods, dependent: :destroy
   has_many :posts, through: :goods
+
+  def update_with_password(params, * options)
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+    update_attributes(params, * options)
+  end
 end
