df95cfbf671dc39ec14ab03fc747d747ff9ec7cf
sato akihide <aki@sato.local>
2019-06-24 14:36:44 +0900

    Fix: seedデータ生成用のコメントアウト

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,6 +1,2 @@
-I18n.config.available_locales = :ja
-I18n.default_locale = :ja
-require 'i18n'
-I18n.locale = :ja
-require 'faker'
-Faker::Config.locale = :en
+# I18n.config.available_locales = :ja
+# I18n.default_locale = :ja
