4f75e4e7bd5f891efbfa151c3c0223b93856020b
sato akihide <aki@sato.local>
2019-06-26 17:31:33 +0900

    Fix: userモデルのテストが不足していたため追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,10 +19,28 @@ RSpec.describe User, type: :model do
 
       context '失敗' do
 
+        it 'nameが空だとバリデーションエラー' do
+          user = FactoryBot.build(:user, name: '')
+          user.valid?
+          expect(user.errors.messages[:name]).to include("を入力してください")
+        end
+
+        it 'nameが10文字以上だとバリデーションエラー' do
+          user = FactoryBot.build(:user, name: 'あ' * 11)
+          user.valid?
+          expect(user.errors.messages[:name]).to include("は10文字以内で入力してください")
+        end
+
         it 'emailが空だとバリデーションエラー' do
           user = FactoryBot.build(:user, email: '')
           user.valid?
           expect(user.errors.messages[:email]).to include("を入力してください")
+        end
+
+        it 'emailが255文字以上だとバリデーションエラー' do
+          user = FactoryBot.build(:user, email: ('a' * 250) + "@co.jp")
+          user.valid?
+          expect(user.errors.messages[:email]).to include("は255文字以内で入力してください")
         end
 
         it 'passwordが空だとバリデーションエラー' do
@@ -31,14 +49,24 @@ RSpec.describe User, type: :model do
           expect(user.errors.messages[:password]).to include("を入力してください")
         end
 
+        it 'passwordが6文字以下だとバリデーションエラー' do
+          user = FactoryBot.build(:user, password: '1' * 5)
+          user.valid?
+          expect(user.errors.messages[:password]).to include("は6文字以上で入力してください")
+        end
+
         it 'password_confirmationが空だとバリデーションエラー' do
           user = FactoryBot.build(:user, password_confirmation: '')
           user.valid?
           expect(user.errors.messages[:password_confirmation]).to include("とパスワードの入力が一致しません")
         end
 
+        it 'passwordとpassword_confirmationが不一致だとバリデーションエラー' do
+          user = FactoryBot.build(:user, password: '123456', password_confirmation: '654321')
+          user.valid?
+          expect(user.errors.messages[:password_confirmation]).to include("とパスワードの入力が一致しません")
+        end
       end
-
     end
   end
 end
