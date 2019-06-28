fe9081c7a35446b2a4b648b4caa146670d9ecee2
sato akihide <aki@sato.local>
2019-06-28 16:06:12 +0900

    Add: ユーザーページに感想を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @books = @user.books.includes(:publication, :taggings).search_book(params[:title], params[:author]).order_new
     @books = @books.tagged_with(params[:category_name]) if params[:category_name].present?
     @books = @books.page(params[:page]).per(PER)
+    @posts = @user.book_posts.page(params[:page]).per(PER)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
   has_many :publications, through: :books
   has_many :comments, dependent: :destroy
   has_many :goods, dependent: :destroy
-  has_many :posts, through: :goods
+  has_many :good_posts, through: :goods, source: :posts
+  has_many :book_posts, through: :books, source: :posts
 
   validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
