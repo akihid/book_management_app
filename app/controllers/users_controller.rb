ccc1a7f1261223b6b2518b6e5fa9cfe3965f4002
sato akihide <aki@sato.local>
2019-10-06 14:37:24 +0900

    Add: 読んでる本、読んでない本の金額表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,22 @@ class UsersController < ApplicationController
     @posts = @user.book_posts
     @comment_posts = @user.comment_posts.includes(:publication, :user)
     @good_posts = @user.good_posts.includes(:publication, :user)
-    @price = @user.books.includes(:publication).sum(:price)
+
+    sum_price
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def sum_price
+    price_hash = @user.books.includes(:publication).group(:read_status).sum(:price)
+    not_read_price = price_hash["not_read"].present? ? price_hash["not_read"] : 0 
+    now_read_price = price_hash["now_read"].present? ? price_hash["now_read"] : 0 
+    
+    @not_read_price = not_read_price + now_read_price
+    @finish_read_price = price_hash["finish_read"].present? ? price_hash["finish_read"] : 0 
   end
 end
